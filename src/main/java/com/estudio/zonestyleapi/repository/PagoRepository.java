6fd183efd6a39e8b0771cc75a0dacb3cbb74aefa
unknown <maycolsantillansanchez433@gmail.com>
2026-04-22 18:24:24 -0500

    Ronni Mostacero y Kevin Sangay: Implementación de repositorios

---
+++ src/main/java/com/estudio/zonestyleapi/repository/PagoRepository.java
@@ -0,0 +1,9 @@
+package com.estudio.zonestyleapi.repository;
+
+import com.estudio.zonestyleapi.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagoRepository extends JpaRepository<Cliente, Long> {
+}
