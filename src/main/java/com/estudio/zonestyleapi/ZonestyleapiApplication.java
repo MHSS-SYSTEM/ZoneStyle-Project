632c648f4fe6f3b7f23c319a22d66b64a6e892e9
unknown <maycolsantillansanchez433@gmail.com>
2026-04-22 18:18:27 -0500

    Junior Cerna: Configuración inicial y conexión MySQL

---
+++ src/main/java/com/estudio/zonestyleapi/ZonestyleapiApplication.java
@@ -0,0 +1,13 @@
+package com.estudio.zonestyleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonestyleapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZonestyleapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estudio/zonestyleapi/ZonestyleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudio.zonestyleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonestyleapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
